8b80b32304a2265c3fd72898e103c6571adebde7
PetStoreSTA2021 <vanina.braga@gmail.com>
2021-09-25 13:20:19 -0300

    alterações da segunda aula curso com a estrutura do While, e uso do If-Elif_Else como um Switch-Case ou Match-Case

--- main.py
+++ main.py
@@ -39,12 +39,57 @@ def brincar_de_plim(fim):
         if numero % 4 == 0:
             print('PLIM!')
         else:
-            print('{:0>5}'.format(numero))
+            print('{:0>3}'.format(numero))
 
 
 #estrutura de identificação / execução do script
 if __name__ == '__main__':
-    print_hi('Vanina')
+
+    resposta = "C"
+
+    while resposta.upper() !='Z':
+
+        print('####################################')
+        print('#                                  #')
+        print('#    M E N U   D E   O P Ç Õ E S   #')
+        print('#                                  #')
+        print('#        1 - Olá Mundo!            #')
+        print('#        2 - Área do Retângulo     #')
+        print('#        3 - Área do Quadrado      #')
+        print('#        4 - Área do Triângulo     #')
+        print('#        5 - Contagem Progressiva  #')
+        print('#        6 - Apoiar Candidato      #')
+        print('#        7 - Brincar de Plim       #')
+        print('#                                  #')
+        print('#        Z - Sair                  #')
+        print('#                                  #')
+        print('####################################')
+
+        resposta = input("Escolha sua opção")
+        print(f'A sua escolha foi: {resposta}')
+
+        if resposta.upper() != 'Z':
+            if resposta == '1':
+                print_hi('Vanina')
+            elif resposta == '2':
+                resultado = calcular_area_do_retangulo(8, 7)
+                print(f'A área do retângulo é de {resultado} m²')
+            elif resposta == '3':
+                resultado = calcular_area_do_quadrado(6)
+                print(f'A área do quadrado é de {resultado} m²')
+            elif resposta == '4':
+                resultado = calcular_area_do_triangulo(5, 8)
+                print(f'A área do triângulo é de {resultado} m²')
+            elif resposta == '5':
+                realizar_contagem_progressiva(10)
+            elif resposta == '6':
+                apoiar_candidato('Murphy', 13)
+            elif resposta == '7':
+                brincar_de_plim(7)
+            else:
+                print("Você edigitou uma opção inválida. escolha uma opção de 1 a 7")
+        else:
+            print("Você escolheu sair. Volte sempre")
 
 #chamar a função de cálculo da área do retângulo
     resultado = calcular_area_do_retangulo(3,4)
@@ -59,10 +104,12 @@ if __name__ == '__main__':
     print(f'A área do triângulo é de {resultado} m²')
 
 #executar uma contagem progressiva
-    realizar_contagem_progressiva(10)
+    realizar_contagem_progressiva(11)
 
 #exibir o nome do candidato varias vezes
     apoiar_candidato('Faker', 100)
 
 #brincar de Plim
     brincar_de_plim(100)
+
+
